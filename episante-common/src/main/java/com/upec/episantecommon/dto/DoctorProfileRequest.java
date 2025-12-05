8fd7471e9ccd46d78aa4fd52e6277672ab165528
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-12-05 01:09:06 +0100

    ref: getters + setters to lombok

--- episante-common/src/main/java/com/upec/episantecommon/dto/DoctorProfileRequest.java
+++ episante-common/src/main/java/com/upec/episantecommon/dto/DoctorProfileRequest.java
@@ -1,7 +1,9 @@
 package com.upec.episantecommon.dto;
 
+import lombok.Data;
 import java.util.UUID;
 
+@Data
 public class DoctorProfileRequest {
     private UUID id;
     private String firstName;
@@ -10,62 +12,4 @@ public class DoctorProfileRequest {
     private String specialty;
     private String rppsNumber;
     private String address;
-
-    // getters + setters
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSpecialty() {
-        return specialty;
-    }
-
-    public void setSpecialty(String specialty) {
-        this.specialty = specialty;
-    }
-
-    public String getRppsNumber() {
-        return rppsNumber;
-    }
-
-    public void setRppsNumber(String rppsNumber) {
-        this.rppsNumber = rppsNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
--- episante-common/src/main/java/com/upec/episantecommon/dto/PatientProfileRequest.java
+++ episante-common/src/main/java/com/upec/episantecommon/dto/PatientProfileRequest.java
@@ -1,7 +1,10 @@
 package com.upec.episantecommon.dto;
 
+import lombok.Data;
+
 import java.util.UUID;
 
+@Data
 public class PatientProfileRequest {
     private UUID id;
     private String firstName;
@@ -9,55 +12,5 @@ public class PatientProfileRequest {
     private String email;
     private String dob;
     private String phone;
-
-    // getters + setters
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
 
